fb6bf0c8a00524fa87cc373ba1012ceee726116e
Angel Angelov <hextwoa@gmail.com>
2016-03-11 00:10:11 +0200

    outline movie struct

--- movies.go
+++ movies.go
@@ -1,4 +1,16 @@
 package mvm
 
+type EpisodeData struct {
+	SeriesTitle  string `json:"series_title"`
+	SeriesYear   uint   `json:"series_year"`
+	SeriesImdbID string `json:"series_imdb_id"`
+	Season       int    `json:"season"`
+	Episode      int    `json:"episode"`
+}
+
 type Movie struct {
+	Episode *EpisodeData `json:"episode_data"`
+	Title   string       `json:"title"`
+	Year    uint         `json:"year"`
+	ImdbID  string       `json:"imdb_id"`
 }
