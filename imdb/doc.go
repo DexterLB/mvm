5b0392899f127a00dd2982ce969ca47200f771a3
Angel Angelov <hextwoa@gmail.com>
2016-03-20 10:34:56 +0200

    format documentation

--- imdb/doc.go
+++ imdb/doc.go
@@ -1,2 +1,6 @@
-// Package imdb provides easy access to publicly available data on IMDB
+// Package imdb provides easy access to publicly available data on IMDB.
+// Items are accessed by their IMDB ID, and all getter methods called
+// on them are lazy (an http request will be made only when data is needed,
+// and this will happen only once). There is also a convenience AllData()
+// method, which fetches all available data at once.
 package imdb
