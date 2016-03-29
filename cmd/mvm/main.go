40df098f31b06fadc18d529a13b5fd733a4af584
Angel Angelov <hextwoa@gmail.com>
2016-03-30 00:14:24 +0300

    change cli library

--- cmd/mvm/main.go
+++ cmd/mvm/main.go
@@ -1,43 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"os"
 
 	"github.com/DexterLB/mvm/config"
 	"github.com/DexterLB/mvm/importer"
 	"github.com/DexterLB/mvm/library"
 	"github.com/cep21/xdgbasedir"
+	"github.com/codegangsta/cli"
 	_ "github.com/jinzhu/gorm/dialects/sqlite"
-	"github.com/voxelbrain/goptions"
 )
 
-type Options struct {
-	ConfigFile string        `goptions:"-c, --config-file, description='path to the configuration file'"`
-	Help       goptions.Help `goptions:"-h, --help, description='show this help message'"`
-
-	goptions.Verbs
-	Import struct {
-		Path string `goptions:"-p, --path, description='path to import (single video file or a folder)', obligatory"`
-	} `goptions:"import"`
-}
-
-func parseOptions() *Options {
-	options := &Options{}
-	goptions.ParseAndFail(options)
-	return options
-}
-
-func parseConfig(options *Options) *config.Config {
+func parseConfig(c *cli.Context) *config.Config {
 	var err error
 
-	if options.ConfigFile == "" {
-		options.ConfigFile, err = xdgbasedir.GetConfigFileLocation("mvm.toml")
+	filename := c.GlobalString("config-file")
+
+	if filename == "" {
+		filename, err = xdgbasedir.GetConfigFileLocation("mvm.toml")
 		if err != nil {
 			log.Fatalf("can't find config file: %s", err)
 		}
 	}
 
-	config, err := config.Load(options.ConfigFile)
+	config, err := config.Load(filename)
 	if err != nil {
 		log.Fatalf("can't load config file: %s", err)
 	}
@@ -56,9 +44,12 @@ func openLibrary(config *config.Config) *library.Library {
 	return library
 }
 
-func main() {
-	options := parseOptions()
-	config := parseConfig(options)
+func runImport(c *cli.Context) {
+	if c.NArg() == 0 {
+		fmt.Fprintf(os.Stderr, "please supply a filename\n")
+	}
+
+	config := parseConfig(c)
 	library := openLibrary(config)
 
 	importer := importer.NewContext(library, config)
@@ -68,5 +59,30 @@ func main() {
 		}
 	}()
 
-	importer.Import([]string{options.Import.Path})
+	importer.Import([]string(c.Args()))
+}
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "mvm"
+	app.Usage = "identify, manipulate and search movies and series"
+
+	app.Commands = []cli.Command{
+		{
+			Name:    "import",
+			Aliases: []string{"imp", "i"},
+			Usage:   "import a video file into the library",
+			Action:  runImport,
+		},
+	}
+
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "config-file, c",
+			Value: "",
+			Usage: "path to the mvm configuration file",
+		},
+	}
+
+	app.RunAndExitOnError()
 }
